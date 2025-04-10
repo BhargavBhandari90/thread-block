a23cbfc0340a4c8a553f38d84dd4954982b98448
Bunty <bn.bhandari90@gmail.com>
2025-04-10 08:18:21 +0530

    Add function exists

--- thread-block.php
+++ thread-block.php
@@ -5,7 +5,8 @@
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            BuntyWP
+ * Author URI:        https://biliplugins.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       thread-block
@@ -13,15 +14,17 @@
  * @package           thread-block
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
-function buntywp_thread_block_block_init() {
-	register_block_type( __DIR__ . '/build/threads-box' );
-	register_block_type( __DIR__ . '/build/thread-item' );
+if ( ! function_exists( 'buntywp_thread_block_block_init' ) ) {
+	/**
+	 * Registers the block using the metadata loaded from the `block.json` file.
+	 * Behind the scenes, it registers also all assets so they can be enqueued
+	 * through the block editor in the corresponding context.
+	 *
+	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
+	 */
+	function buntywp_thread_block_block_init() {
+		register_block_type( __DIR__ . '/build/threads-box' );
+		register_block_type( __DIR__ . '/build/thread-item' );
+	}
 }
 add_action( 'init', 'buntywp_thread_block_block_init' );
